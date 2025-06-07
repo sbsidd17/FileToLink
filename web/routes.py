aea68b8612b65ba269d7f1268fef971ef89c28a1
RAZi <119275831+MeeRazi@users.noreply.github.com>
2025-06-07 08:46:21 +0530

    updated

--- web/routes.py
+++ web/routes.py
@@ -9,6 +9,11 @@ from web.utils.render_template import render_page
 
 routes = web.RouteTableDef()
 
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     raise web.HTTPFound('https://telegram.me/botsync')
