37e4fa9586469f67b2963f347171c92d87ee4044
RAZi <119275831+MeeRazi@users.noreply.github.com>
2025-06-07 08:43:26 +0530

    updated

--- info.py
+++ info.py
@@ -12,4 +12,4 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 PORT = int(os.getenv('PORT', '8080'))
 BIN_CHANNEL = int(os.getenv("BIN_CHANNEL"))
-STREAM_URL = os.getenv("URL")
+STREAM_URL = os.getenv("STREAM_URL")
