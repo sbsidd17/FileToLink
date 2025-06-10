4dcf1e314a93dfd7deac44ba6fe7b4dfedc60a35
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2025-06-10 22:34:35 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -19,9 +19,10 @@ async def private_receive_handler(client, message):
     file_name = file_id.file_name.replace(" ", "_")
     online = f"{STREAM_URL}/watch/{msg.id}/{file_name}"
     download = f"{STREAM_URL}/download/{msg.id}/{file_name}"
+    p_play = f"playit://playerv2/video?url={online}
 
     await message.reply_text(
-        text=f"<b>Here Is Your Streamable Link\n\nFile Name</b>:\n<code>{file_name}</code>\n\n<b>Powered By - <a href=https://t.me/sdbots1>©sdBots</a></b>",
+        text=f"<b>Here Is Your Streamable Link\n\nFile Name</b>:\n<code>{file_name}</code>\n\n<b>Powered By - <a href=https://t.me/sdbots1>©sdBots</a></b>\n\n<b>Play Using - <a href={p_play}>PlayIt</a></b>",
         reply_markup=InlineKeyboardMarkup(
         [
             [
