17b0437fa0b9e4e1dadd747d090f492acc68a695
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2025-06-10 21:11:47 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -17,8 +17,8 @@ async def private_receive_handler(client, message):
         caption=f"**File Name:** {file_id.file_name}\n\n**Requested By:** {message.from_user.mention}")
 
     file_name = file_id.file_name.replace("_", " ").replace(".", " ")
-    online = f"{STREAM_URL}/watch/{msg.id}"
-    download = f"{STREAM_URL}/download/{msg.id}"
+    online = f"{STREAM_URL}/watch/{msg.id}/{file_name}"
+    download = f"{STREAM_URL}/download/{msg.id}/{file_name}"
 
     await message.reply_text(
         text=f"<b>Here Is Your Streamable Link\n\nFile Name</b>:\n<code>{file_name}</code>\n\n<b>Powered By - <a href=https://t.me/sdbots1>©sdBots</a></b>",
